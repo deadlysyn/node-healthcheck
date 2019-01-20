6f72719bd2ca26751cfdee9315c09c2c983105b4
Mike Hoskins <unixdoes@gmail.com>
2019-01-19 21:28:33 -0500

    cleanup

--- app.js
+++ app.js
@@ -1,56 +1,43 @@
-const express = require('express'),
-    app = express(),
-    fs = require('fs'),
-    path = require('path'),
-    m = require('./middleware')
+const express = require('express');
+const m = require('./middleware');
 
-const configFile = path.join(__dirname, process.env.CONF)
-const ip = process.env.IP || '0.0.0.0'
-const port = parseInt(process.env.PORT, 10) || 3000
+const ip = process.env.IP || '0.0.0.0';
+const port = parseInt(process.env.PORT, 10) || 3000;
+
+const app = express();
+
+// const fs = require('fs');
+// const path = require('path');
+// const configFile = path.join(__dirname, process.env.CONF);
 
 app.use((req, res, next) => {
-    if (!req.app.locals.testResults) {
-        // hold aggregated test results
-        req.app.locals.testResults = {}
-    }
-    next()
-})
+  express.static('public');
 
-// build list of tests to run based on config
-app.locals.conf = JSON.parse(fs.readFileSync(configFile, {encoding: 'utf-8'}))
-var tests = []
+  if (!req.app.locals.testResults) {
+    req.app.locals.testResults = {};
+  }
 
-if (app.locals.conf.testMongo) {
-    tests.push(m.testMongo)
-}
-if (app.locals.conf.testMysql) {
-    tests.push(m.testMysql)
-}
-if (app.locals.conf.testPostgres) {
-    tests.push(m.testPostgres)
-}
-if (app.locals.conf.testRabbit) {
-    tests.push(m.testRabbit)
-}
-if (app.locals.conf.testRedis) {
-    tests.push(m.testRedis)
-}
-if (tests.length == 0) {
-    req.app.locals.testResults = { 'message': 'No tests enabled.' }
-}
+  if (!req.app.locals.statusCode) {
+    req.app.locals.statusCode = 200;
+  }
+
+  next();
+});
 
 // routes
-app.get('/', tests, (req, res, next) => {
-    res.json({
-        'timestamp': new Date().toJSON(),
-        'results': req.app.locals.testResults
-    })
-})
+app.get('/', (req, res, next) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.get('/healthcheck', m.healthcheck, (req, res, next) => {
+  // timestamp: new Date().toJSON(),
+  res.status(req.app.locals.statusCode).json({ results: req.app.locals.testResults });
+});
 
 app.all('*', (req, res, next) => {
-    res.status(404).json({ 'message': 'Page not found.' })
-})
+  res.status(404).json({ message: 'Page not found.' });
+});
 
 app.listen(port, ip, _ => {
-    console.log('listening on ' + ip + ':' + port)
-})
+  console.log(`listening on ${ip}:${port}`);
+});
